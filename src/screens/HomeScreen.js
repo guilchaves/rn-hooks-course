f92a12fe60bb18bec0d1c0da6a622e6adf82da69
guilherme <guilgchaves@protonmail.com>
2022-07-15 18:39:13 -0300

    Section 5 - Building Reusable Components

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,10 +4,16 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 const HomeScreen = ({ navigation }) => {
   return (
     <View>
-      <Text style={styles.text}>Hi There</Text>
-      <Button title="Go to Components Demo" onPress={() => navigation.navigate('Component')} />
-      <Text></Text>
-      <Button title="Go to List Demo" onPress={() => navigation.navigate('List')} />
+      <Text style={styles.text}>Home</Text>
+      <View style={styles.margin}>
+        <Button title="Go to Components Demo" onPress={() => navigation.navigate('Component')} />
+      </View>
+      <View style={styles.margin}>
+        <Button title="Go to List Demo" onPress={() => navigation.navigate('List')} />
+      </View>
+      <View style={styles.margin}>
+        <Button title="Go to Image Demo" onPress={() => navigation.navigate('Image')} />
+      </View>
     </View>
   );
 };
@@ -15,6 +21,9 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+  },
+  margin: {
+    margin: 10,
   },
 });
 
