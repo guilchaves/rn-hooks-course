80f5afccd5810c2fd1ba055dd8ed079638f35144
guilherme <guilgchaves@protonmail.com>
2022-07-19 19:06:21 -0300

    Section 9 - Using Outside API's

--- 2-restaurant-search-app/src/screens/SearchScreen.js
+++ 2-restaurant-search-app/src/screens/SearchScreen.js
@@ -1,18 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get('/search', { params: { limit: 50, term, location: 'sao paulo' } });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
 
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log('term was submitted')}
-      />
-      <Text>Search Screen</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have fo {results.length} results</Text>
     </View>
   );
 };
