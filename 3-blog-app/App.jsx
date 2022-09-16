d657f0ffd07a30b641bb2366241194e85ed8c78c
guilherme <guilgchaves@protonmail.com>
2022-09-16 17:16:56 -0300

    add index screen and navigation

---
+++ 3-blog-app/App.jsx
@@ -0,0 +1,22 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import IndexScreen from './src/screens/IndexScreen';
+
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+  },
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs',
+    },
+  }
+);
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />;
+};
