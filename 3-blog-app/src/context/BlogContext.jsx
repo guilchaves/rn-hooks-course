912fb4426e3dc12d93c18dc4753734a2cba49d75
guilherme <guilgchaves@protonmail.com>
2022-09-16 18:04:20 -0300

    render list of posts

--- 3-blog-app/src/context/BlogContext.jsx
+++ 3-blog-app/src/context/BlogContext.jsx
@@ -8,5 +8,14 @@ const BlogContext = React.createContext();
  * @returns Provider with children components
  */
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+  const blogPosts = [
+    { title: 'Blog Post #1' },
+    { title: 'Blog Post #2' },
+    { title: 'Blog Post #3' },
+    { title: 'Blog Post #4' },
+  ];
+
+  return <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>;
 };
+
+export default BlogContext;
--- 3-blog-app/src/screens/IndexScreen.jsx
+++ 3-blog-app/src/screens/IndexScreen.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { InteractionManager, StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
+import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+  const blogPosts = useContext(BlogContext);
+
   return (
     <View>
       <Text>Index Screen</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
